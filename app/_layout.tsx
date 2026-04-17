e1f1c8a5ee43e34977aa5dd90dcbf6593ed6c7fa
Chocobo11218 <theerisara11218@gmail.com>
2026-04-17 11:47:44 +0700

    polish inspect ui

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,7 +6,12 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 export default function RootLayout() {
   const [loaded, error] = useFonts({
     "SaoChingcha-Bold": require("../assets/fonts/SaoChingcha-Bold.otf"),
+    "Anuphan-Thin": require("../assets/fonts/Anuphan-Thin.ttf"),
+    "Anuphan-ExtraLight": require("../assets/fonts/Anuphan-ExtraLight.ttf"),
+    "Anuphan-Light": require("../assets/fonts/Anuphan-Light.ttf"),
     "Anuphan-Regular": require("../assets/fonts/Anuphan-Regular.ttf"),
+    "Anuphan-Medium": require("../assets/fonts/Anuphan-Medium.ttf"),
+    "Anuphan-SemiBold": require("../assets/fonts/Anuphan-SemiBold.ttf"),
     "Anuphan-Bold": require("../assets/fonts/Anuphan-Bold.ttf"),
   });
 
